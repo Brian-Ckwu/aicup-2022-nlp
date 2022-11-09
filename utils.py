a245e6df5c6add879d2674919ab96a3e2ba65fa4
Brian-Ckwu <sonny40920710@gmail.com>
2022-11-09 11:06:44 +0800

    add load_json() to utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
-# Utility function # TODO: put this in a utils.py file
+import json
+from pathlib import Path
+
+def load_json(file: str):
+    return json.loads(Path(file).read_bytes())
+
 def longestCommonSubsequence(text1: list, text2: list) -> int:
     if len(text2) > len(text1):
         text1, text2 = text2, text1
@@ -12,3 +17,8 @@ def longestCommonSubsequence(text1: list, text2: list) -> int:
                 lcs[i % 2][j] = max(lcs[(i-1) % 2][j], lcs[i % 2][j-1])
 
     return lcs[len(text1) % 2][len(text2)]
+
+# for unit testing
+if __name__ == "__main__":
+    obj = load_json("./dataset/splitIds__splitBy-id_stratifyBy-s_train-0.6_valid-0.2_test-0.2_seed-42.json")
+    print(obj)
