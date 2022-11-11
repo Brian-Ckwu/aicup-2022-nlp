c3ec914f34d0613d4e6eeaf7f03421f07b34701d
Brian-Ckwu <sonny40920710@gmail.com>
2022-11-12 01:04:49 +0800

    modify eval_script.py: fix bugs

--- eval_script.py
+++ eval_script.py
@@ -27,8 +27,10 @@ def parse_args() -> Namespace:
 if __name__ == "__main__":
     args = parse_args()
 
-    pred_df = pd.read_csv(args.prediction)
-    ans_df = pd.read_csv(args.answer)
+    pred_df = pd.read_csv(args.prediction, names=["id", "q'", "r'"])
+    ans_df = pd.read_csv(args.answer, names=["id", "q'", "r'"])
+    if len(pred_df) != len(ans_df.groupby("id").size()):
+        raise ValueError("The prediction file must have the same number of rows as the number of unique IDs in the answer file")
     
     lcs_df = compute_lcs_scores(pred_df, ans_df) # has len(ans_df) rows of lcs_q' and lcs_r'
     final_score = compute_final_score(lcs_df) # derive the final score by "1/2N (\sum_i^N(max_j(score_q' + score_r')))"
