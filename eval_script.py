3eb6f43ed3d339a946096ea139b798471ebba6a9
Brian-Ckwu <sonny40920710@gmail.com>
2022-11-12 00:44:57 +0800

    add evaluation function (aggregated lcs scores

---
+++ eval_script.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from pathlib import Path
+from argparse import Namespace, ArgumentParser
+
+from utils import compute_lcs_scores, compute_final_score
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "--prediction",
+        type=Path,
+        help="Path to the prediction file (csv).",
+        required=True
+    )
+    parser.add_argument(
+        "--answer",
+        type=Path,
+        help="Path to the answer file (csv).",
+        required=True
+    )
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    pred_df = pd.read_csv(args.prediction)
+    ans_df = pd.read_csv(args.answer)
+    
+    lcs_df = compute_lcs_scores(pred_df, ans_df) # has len(ans_df) rows of lcs_q' and lcs_r'
+    final_score = compute_final_score(lcs_df) # derive the final score by "1/2N (\sum_i^N(max_j(score_q' + score_r')))"
+    print(f"Final test score: {final_score}")
+
+    # TODO: save the final_score to corresponding model checkpoint's folder
--- utils.py
+++ utils.py
@@ -2,7 +2,11 @@ import json
 from typing import Dict
 from pathlib import Path
 
+import pandas as pd
+
 from transformers import EvalPrediction
+
+from preprocess import Preprocessor
 
 def load_json(file: str):
     return json.loads(Path(file).read_bytes())
@@ -31,6 +35,45 @@ def longestCommonSubsequence(text1: list, text2: list) -> int:
                 lcs[i % 2][j] = max(lcs[(i-1) % 2][j], lcs[i % 2][j-1])
 
     return lcs[len(text1) % 2][len(text2)]
+
+def compute_lcs_score(pred: list, ans: list) -> float:
+    intersection = longestCommonSubsequence(pred, ans)
+    union = len(pred) + len(ans) - intersection
+    lcs_score = intersection / union
+    if (lcs_score < 0) or (lcs_score) > 1:
+        raise ValueError("LCS score must be between 0 and 1")
+    return lcs_score
+
+def compute_lcs_scores(pred_df: pd.DataFrame, ans_df: pd.DataFrame) -> pd.DataFrame:
+    ids, qp_scores, rp_scores = list(), list(), list()
+    for _, prow in pred_df.iterrows():
+        pid, qp_pred, rp_pred = prow["id"], prow["q'"], prow["r'"]
+        qp_pred, rp_pred = [Preprocessor.nltk_tokenize(pred) for pred in [qp_pred, rp_pred]]
+        ans_rows = ans_df[ans_df.id == pid]
+
+        for _, arow in ans_rows.iterrows():
+            qp_ans, rp_ans = arow["q'"], arow["r'"]
+            qp_ans, rp_ans = [Preprocessor.nltk_tokenize(ans) for ans in [qp_ans, rp_ans]]
+            qp_score, rp_score = compute_lcs_score(qp_pred, qp_ans), compute_lcs_score(rp_pred, rp_ans)
+
+            for item, l in zip([pid, qp_score, rp_score], [ids, qp_scores, rp_scores]):
+                l.append(item)
+
+    assert ids == ans_df.id.tolist()
+    lcs_df = pd.DataFrame(data={
+        "id": ids,
+        "qp_scores": qp_scores,
+        "rp_scores": rp_scores
+    })
+    return lcs_df
+
+def compute_final_score(lcs_df: pd.DataFrame) -> float:
+    lcs_df["total_scores"] = lcs_df["qp_scores"] + lcs_df["rp_scores"]
+    max_scores = lcs_df.groupby("id")["total_scores"].max()
+    final_score = max_scores.sum() / (2 * len(max_scores))
+    if (final_score < 0) or (final_score > 1):
+        raise ValueError("The final score must be between 0 and 1, please check the implementation.")
+    return final_score
 
 compute_metrics_funcs = {
     "token_acc": compute_token_acc
